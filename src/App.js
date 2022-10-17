e9bdef018b505dc2647e53c5507fd918d132041f
Srinivas Gangaraju <gangarajusrinivas25@gmail.com>
2022-10-17 15:03:25 +0530

    add graphql changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import CharactersList from "./pages/CharactersList";
 import { Route, Routes } from "react-router";
 import Character from "./pages/Character";
 import Search from "./pages/Search";
-import Bids from "./pages/Bids";
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route strict exact path="/" element={<Bids />} />
+        <Route strict exact path="/" element={<CharactersList />} />
         <Route strict exact path="/:id" element={<Character />} />
         <Route strict exact path="/search" element={<Search />} />
       </Routes>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
-//https://rickandmortyapi.com/graphql
+
 const client = new ApolloClient({
-  uri: "http://35.174.109.199:8000/subgraphs/name/persistence-bl-subgraph",
+  uri: "https://rickandmortyapi.com/graphql",
   cache: new InMemoryCache(),
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -20,7 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
